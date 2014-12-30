a0feb971ab791972c7ed702f2c991610dd1f3f92
David McNicol <david@mcnicks.org>
2014-12-30 11:30:58 +0000

    Changed class_variable_get/set to be called as class methods

--- lib/puppet/provider/gitlab.rb
+++ lib/puppet/provider/gitlab.rb
@@ -3,18 +3,18 @@ class Puppet::Provider::Gitlab < Puppet::Provider
   protected
   
     def token
-      class_variable_get(:@@token)
+      Puppet::Provider::Gitlab.class_variable_get(:@@token)
     end
   
     def token=(value)
-      class_variable_set(:@@token, value)
+      Puppet::Provider::Gitlab.class_variable_set(:@@token, value)
     end
   
     def api_url
-      class_variable_get(:@@api_url)
+      Puppet::Provider::Gitlab.class_variable_get(:@@api_url)
     end
   
     def api_url=(value)
-      class_variable_set(:@@api_url, value)
+      Puppet::Provider::Gitlab.class_variable_set(:@@api_url, value)
     end
 end
