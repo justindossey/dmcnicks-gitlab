2b5def7c8241cc36eeabf5d7af6a4ac255bfa00f
David McNicol <david@mcnicks.org>
2014-12-30 11:55:12 +0000

    Added ensurable methods so that they are available for all subclasses

--- lib/puppet/provider/gitlab.rb
+++ lib/puppet/provider/gitlab.rb
@@ -1,23 +1,39 @@
 class Puppet::Provider::Gitlab < Puppet::Provider
 
-  class_variable_set(:@@token, nil)
-  class_variable_set(:@@api_url, nil)
+  # The create, destroy and exists? methods provide the basic ensurable
+  # functionality for all of the Gitlab providers. Since we are using
+  # prefetch/flush, these methods only have to manage the @propety_hash.
 
-  protected
-  
-    def token
-      Puppet::Provider::Gitlab.class_variable_get(:@@token)
+  def create
+    @property_hash[:ensure] = :present
+    self.class.resource_type.validproperties.each do |property|
+      if val = resource.should(property)
+        @property_hash[property] = val
+      end
     end
-  
-    def token=(value)
-      Puppet::Provider::Gitlab.class_variable_set(:@@token, value)
-    end
-  
-    def api_url
-      Puppet::Provider::Gitlab.class_variable_get(:@@api_url)
-    end
-  
-    def api_url=(value)
-      Puppet::Provider::Gitlab.class_variable_set(:@@api_url, value)
-    end
+  end
+
+  def destroy
+    @property_hash[:ensure] = :absent
+  end
+
+  def exists?
+    @property_hash[:ensure] != :absent
+  end
+
+  def token
+    Puppet::Provider::Gitlab.class_variable_get(:@@token)
+  end
+
+  def token=(value)
+    Puppet::Provider::Gitlab.class_variable_set(:@@token, value)
+  end
+
+  def api_url
+    Puppet::Provider::Gitlab.class_variable_get(:@@api_url)
+  end
+
+  def api_url=(value)
+    Puppet::Provider::Gitlab.class_variable_set(:@@api_url, value)
+  end
 end
