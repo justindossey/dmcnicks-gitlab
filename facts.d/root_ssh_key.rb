e31ff591a83c0c37ca51e669190b791457cdd461
David McNicol <david@mcnicks.org>
2014-12-30 22:39:42 +0000

    Fixed exists brackets (for tidiness)

--- facts.d/root_ssh_key.rb
+++ facts.d/root_ssh_key.rb
@@ -1,7 +1,7 @@
 Facter.add('root_ssh_key') do
   pubkey = [ '/root/.ssh/id_rsa.pub', '/root/.ssh/id_dsa.pub' ]
   pubkey.each do |key|
-    if File.exists? key
+    if File.exists?(key)
       setcode do
         File.read(key).split[1]
       end
