f9c1154abfe58a8fa23c7f426f5059ff2f95653b
David McNicol <david@mcnicks.org>
2014-12-30 23:59:01 +0000

    Setting new deploy key

--- manifests/config.pp
+++ manifests/config.pp
@@ -89,6 +89,6 @@ class gitlab::config (
     ensure       => 'present',
     session      => 'config',
     project      => 'My New Project',
-    key          => 'ssh-rsa AABAB3NzaC1yc2EAAAADAQABAAABAQC1nj25yO+Jgm6/YQTHgEwkjJMgn5q+5vX22CQHdiO1/Dba5TKjr9w0T/SG94yi+l8qJQh/xhrwcvdiQc16V6ltfST1J3yZ0Rad+RUTD1pbWax376HzmtK7a6qZnSkAKTQ+WrQDOqtsPasOT4rLrW8DXUHbXLBTO5X6BydVcmG21sxHU4ikWL3dDI6qkzUd3b3u8EWBSMsirVEiG5hOScSZVy5mzfCGFz0hlx/9gjjbS/mjUAmIh29M6v+DnTkhzNsVeNO+DCWjgVh1nbW8GYDUD4C67seVCzVYNR/4IhLIuj208mJ/ZWZRxOUwj5fC+ApmWyMjN6v2iVxUs/HotxeZ root@agentdebian.local'
+    key          => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC94v1rpU/pJ1+coP0gThjMpgpvHVCMC/3YX+91SxSv3tpAVnUCi4yUNxLagA/0xQX9+WyWP3hHGo64DnangLZu/1LP8DojqczGVRioYBwpk4KiReCBEJ/m5BdtxPMTHpuh/vK4wTqylwYUgEr0CoZyQxy12wcIDv+CLZj4WiE6yZDodOTPswjidbjkEFZML/n8RFDK8Erq9RZSZZr8EpCSuLOsfMPSTVS5gk1F5X/1ZV1TguE2zVhm9N1MkIZdUn9XxwpabVBEr5B9HLLflch/8A2F+99DHXmpqodlXMaVIkM8T76AoYuZhDRbEoJy/b5nmWRC5irYy8nkgaVzZ83F root@agentdebian'
   }
 }
