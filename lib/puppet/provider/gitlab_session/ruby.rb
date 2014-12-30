f14d95840b66b4d51e380967cb80246aa9f56a1e
David McNicol <david@mcnicks.org>
2014-12-30 15:15:53 +0000

    Going back to basics to find the munge problem

--- lib/puppet/provider/gitlab_session/ruby.rb
+++ lib/puppet/provider/gitlab_session/ruby.rb
@@ -8,4 +8,15 @@ Puppet::Type.type(:gitlab_session).provide(
 
   desc 'Default provider for gitlab_session type'
 
+  # Make sure that the rest-client package is available.
+
+  confine :true => begin
+    begin
+      require 'rest_client'
+      true
+    rescue LoadError
+      false
+    end
+  end  
+
 end
