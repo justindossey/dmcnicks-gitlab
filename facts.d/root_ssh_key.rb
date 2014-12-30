b9a783bd26ff681c04fd6baec99219430c7eb992
David McNicol <david@mcnicks.org>
2014-12-30 19:50:19 +0000

    Removed debugging messages

--- facts.d/root_ssh_key.rb
+++ facts.d/root_ssh_key.rb
@@ -1,9 +1,7 @@
 Facter.add('root_ssh_key') do
-  puts "FACTER"
   pubkey = '/root/.ssh/id_rsa.pub', '/root/.ssh/id_dsa.pub'
   pubkey.each do |key|
     if File.exists? key
-      puts "FOUND KEY " << key
       setcode do
         File.read(key).split[1]
       end
