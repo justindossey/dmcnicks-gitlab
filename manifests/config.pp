30c7c38a7ac8443931b2d898e376f6605f871c07
David McNicol <david@mcnicks.org>
2014-12-30 23:49:16 +0000

    Created gitlab_deploy_key type

--- manifests/config.pp
+++ manifests/config.pp
@@ -84,4 +84,11 @@ class gitlab::config (
     session => 'config',
     namespace => 'My Group'
   }
+
+  gitlab_deploy_key { 'my deploy key':
+    ensure       => 'present',
+    session      => 'config',
+    project      => 'My New Project',
+    key          => 'ssh-rsa AABAB3NzaC1yc2EAAAADAQABAAABAQC1nj25yO+Jgm6/YQTHgEwkjJMgn5q+5vX22CQHdiO1/Dba5TKjr9w0T/SG94yi+l8qJQh/xhrwcvdiQc16V6ltfST1J3yZ0Rad+RUTD1pbWax376HzmtK7a6qZnSkAKTQ+WrQDOqtsPasOT4rLrW8DXUHbXLBTO5X6BydVcmG21sxHU4ikWL3dDI6qkzUd3b3u8EWBSMsirVEiG5hOScSZVy5mzfCGFz0hlx/9gjjbS/mjUAmIh29M6v+DnTkhzNsVeNO+DCWjgVh1nbW8GYDUD4C67seVCzVYNR/4IhLIuj208mJ/ZWZRxOUwj5fC+ApmWyMjN6v2iVxUs/HotxeZ root@agentdebian.local'
+  }
 }
