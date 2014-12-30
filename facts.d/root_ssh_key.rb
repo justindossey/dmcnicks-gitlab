64435d77131b7a9037700fdcfd627b47e3c21684
David McNicol <david@mcnicks.org>
2014-12-30 22:32:58 +0000

    Fixed permissions. Added array brackets.]

--- facts.d/root_ssh_key.rb
+++ facts.d/root_ssh_key.rb
@@ -1,5 +1,5 @@
 Facter.add('root_ssh_key') do
-  pubkey = '/root/.ssh/id_rsa.pub', '/root/.ssh/id_dsa.pub'
+  pubkey = [ '/root/.ssh/id_rsa.pub', '/root/.ssh/id_dsa.pub' ]
   pubkey.each do |key|
     if File.exists? key
       setcode do
