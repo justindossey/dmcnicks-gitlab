67050aa3a77d83299fe204511e6462295e327960
David McNicol <david@mcnicks.org>
2014-12-30 11:26:28 +0000

    Changed all access to class variables to use class_variable_get/set method

--- lib/puppet/provider/gitlab.rb
+++ lib/puppet/provider/gitlab.rb
@@ -3,18 +3,18 @@ class Puppet::Provider::Gitlab < Puppet::Provider
   protected
   
     def token
-      @@token
+      class_variable_get(:@@token)
     end
   
     def token=(value)
-      @@token = value
+      class_variable_set(:@@token, value)
     end
   
     def api_url
-      @@api_url
+      class_variable_get(:@@api_url)
     end
   
     def api_url=(value)
-      @@api_url = value
+      class_variable_set(:@@api_url, value)
     end
 end
