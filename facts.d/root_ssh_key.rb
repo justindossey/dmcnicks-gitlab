f9976ffb0b613372240b4dca7c4a33c0ab90246c
David McNicol <david@mcnicks.org>
2014-12-31 23:07:22 +0000

    Re-adding

---
+++ facts.d/root_ssh_key.rb
@@ -0,0 +1,10 @@
+Facter.add('root_ssh_key') do
+  pubkey = [ '/root/.ssh/id_rsa.pub', '/root/.ssh/id_dsa.pub' ]
+  pubkey.each do |key|
+    if File.exists?(key)
+      setcode do
+        File.read(key).split[1]
+      end
+    end
+  end
+end
