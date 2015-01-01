74f90ce95dd51b677693dcefe3f9998418256357
David McNicol <david@mcnicks.org>
2015-01-01 08:03:30 +0000

    Changed to executable shell script

--- facts.d/gitlab_root_pubkey.rb
+++ facts.d/gitlab_root_pubkey.rb
@@ -1,10 +1,14 @@
-Facter.add('gitlab_root_pubkey') do
-  pubkey = [ '/root/.ssh/id_rsa.pub', '/root/.ssh/id_dsa.pub' ]
-  pubkey.each do |key|
-    if File.exists? key
-      setcode do
-        File.read(key).split[1]
-      end
-    end
-  end
-end
+#!/bin/sh
+
+PUBKEYS="id_rsa.pub id_dsa.pub"
+SSHDIR="/root/.ssh"
+
+for KEYFILE in $PUBKEYS
+do
+  if [ -f "$SSHDIR/$KEYFILE" ]
+  then
+    KEY=`cat $SSHDIR/$KEYFILE`
+    echo "gitlab_root_pubkey: $KEY"
+    exit
+  fi
+done
