6c2d460bbaf4925d1719cd657aa333bef04ffdc1
David McNicol <david@mcnicks.org>
2015-01-01 08:09:11 +0000

    Moving fact into lib/facter

---
+++ lib/facter/gitlab_root_pubkey.rb
@@ -0,0 +1,10 @@
+Facter.add('gitlab_root_pubkey') do
+  pubkey = [ '/root/.ssh/id_rsa.pub', '/root/.ssh/id_dsa.pub' ]
+  pubkey.each do |key|
+    if File.exists? key
+      setcode do
+        File.read(key).split[1]
+      end
+    end
+  end
+end
