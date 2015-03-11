509a638e60c27acece3c8e5d1c7f411d81a6f9ff
Justin Dossey <justin.dossey@newcontext.com>
2015-03-11 12:51:39 -0700

    Add necessary info to init spec

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,16 @@
 require 'spec_helper'
 describe 'gitlab' do
+  let(:facts) do
+    { operatingsystem: 'CentOS',
+      operatingsystemmajrelease: '6',
+      osfamily: 'RedHat'
+    }
+  end
 
   context 'with defaults for all parameters' do
+    let(:params) do
+      { admin_password: 'secret' }
+    end
     it { should contain_class('gitlab') }
   end
 end
